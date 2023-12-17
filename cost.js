572c880668765df9bec2403733a6aba76a3782e0
Kevin Tran <77633718+Hdz2001@users.noreply.github.com>
2023-12-17 05:29:44 -0600

    Add files via upload

--- cost.js
+++ cost.js
@@ -1,5 +1,4 @@
-const ingredients = [
-]
+const ingredients = []
 
 let totalCost = 0;
 const index = document.querySelector(".id");
@@ -8,7 +7,7 @@ const quantity = document.querySelector(".quantity");
 const total = document.querySelector(".total");
 const realQuantity = document.querySelector(".realQuantity");
 const realTotal = document.querySelector(".realTotal");
-/*FORMULA: total*realQuantity/quantity*/
+/*FORMULA: total * realQuantity / quantity*/
 
 function showItem(){
     index.innerHTML = `<h3>STT</h3>`;
@@ -22,20 +21,51 @@ function showItem(){
     for (let i=0; i<ingredients.length; i++){
         let j = ingredients[i];
         let totalX = j.total * j.realQuantity / j.quantity;
-        totalCost += totalX;
 
         index.innerHTML += `<p>${i+1}</p>`
         nameX.innerHTML += `<p>${j.name}</p>`
         quantity.innerHTML += `<p>${j.quantity}</p>`
-        total.innerHTML += `<p>${j.total.toLocaleString('en', {useGrouping:true})}đ</p>`
+        total.innerHTML += `<p>${parseInt(j.total).toLocaleString('en', {useGrouping:true})}đ</p>`
         realQuantity.innerHTML += `<p>${j.realQuantity}</p>`
-        realTotal.innerHTML += `<p>${totalX.toLocaleString('en', {useGrouping:true})}đ</p>`
+
+        if (j.quantity == '' || j.realQuantity == ''){
+            realTotal.innerHTML += `<p>${parseInt(j.total).toLocaleString('en', {useGrouping:true})}đ</p>`
+            totalCost += parseInt(j.total);
+        }
+        else{
+            realTotal.innerHTML += `<p>${totalX.toLocaleString('en', {useGrouping:true})}đ</p>`
+            totalCost += totalX;
+        }
     }
 
     document.querySelector(".totalCost").innerHTML = `Total Cost: ${totalCost.toLocaleString('en', {useGrouping:true})}đ`;
 }
 
 showItem();
+
+
+const calculate = document.querySelector(".calcItem button");
+
+calculate.addEventListener("click", ()=> {
+    let doneWeight = document.getElementById('doneWeight').value;
+    let sellQuantity = document.getElementById('sellQuantity').value;
+
+    if (doneWeight != '' && sellQuantity != ''){
+        let totalSell = totalCost/doneWeight*sellQuantity;
+
+        /*
+        <ul>
+            <li> Số cân 1 cái: ${weightSingle.toFixed(3)}g </li>
+            <li> Số tiền 1 cái: ${costSingle.toFixed(3).toLocaleString('en', {useGrouping:true})}đ </li> 
+        </ul>  
+        */
+
+        document.querySelector(".resultQuantity").innerHTML =
+        `
+        <h3>Giá tiền: ${totalSell.toFixed(3).toLocaleString('en', {useGrouping:true})}đ </h3>       
+        `
+    }
+})
 
 const addItem = document.querySelector(".addItem button");
 
@@ -45,37 +75,22 @@ addItem.addEventListener("click", ()=> {
     let totalItem = document.getElementById('totalItem').value;
     let realQuantityItem = document.getElementById('realQuantityItem').value;
 
-    if (nameItem != '' && quantityItem != '' && totalItem != '' && realQuantityItem != ''){
-        ingredients.push({
+    if ((nameItem != '' && quantityItem == '' && totalItem != '' && realQuantityItem == '') || (nameItem != '' && quantityItem != '' && totalItem != '' && realQuantityItem != '')){
+        ingredients.push(
+        {
             name: nameItem,
             quantity: quantityItem,
             total: totalItem,
-            realQuantity: realQuantityItem,
+            realQuantity: realQuantityItem
         })
         showItem();
+
+        document.getElementById('nameItem').value = '';
+        document.getElementById('totalItem').value = '';
+        document.getElementById('quantityItem').value = '';
+        document.getElementById('realQuantityItem').value = '';
     }
-})
-
-const calculate = document.querySelector(".doneQuantity button");
-
-calculate.addEventListener("click", ()=> {
-    let doneNumber = document.getElementById('doneNumber').value;
-    let doneWeight = document.getElementById('doneWeight').value;
-    let sellQuantity = document.getElementById('sellQuantity').value;
-
-    if (doneNumber != '' && doneWeight != '' && sellQuantity != ''){
-        let costSingle = totalCost/doneNumber;
-        let weightSingle = doneWeight/doneNumber;
-        let totalSell = totalCost/doneWeight*sellQuantity;
-        document.querySelector(".resultQuantity").innerHTML =
-        `
-        <ul>
-            <li> Số cân 1 cái: ${weightSingle.toFixed(3)}g </li>
-            <li> Số tiền 1 cái: ${costSingle.toFixed(3).toLocaleString('en', {useGrouping:true})}đ </li> 
-        </ul>  
-
-        <h3>Tổng bán: ${totalSell.toFixed(3).toLocaleString('en', {useGrouping:true})}đ </h3> 
-        
-        `
-    }
+    
+    console.log("ING")
+    console.log(ingredients)
 })
