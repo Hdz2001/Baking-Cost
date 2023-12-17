f45c29b45397a543d87374ea02d9b2a1d673fe08
Kevin Tran <77633718+Hdz2001@users.noreply.github.com>
2023-12-17 09:36:00 -0600

    Add files via upload

--- cost.js
+++ cost.js
@@ -1,6 +1,8 @@
-const ingredients = []
+let ingredients = []
 
 let totalCost = 0;
+let editing = false;
+const edit = document.querySelector(".edit");
 const index = document.querySelector(".id");
 const nameX = document.querySelector(".name");
 const quantity = document.querySelector(".quantity");
@@ -10,6 +12,7 @@ const realTotal = document.querySelector(".realTotal");
 /*FORMULA: total * realQuantity / quantity*/
 
 function showItem(){
+    edit.innerHTML = `<h3>Chỉnh sửa</h3>`;
     index.innerHTML = `<h3>STT</h3>`;
     nameX.innerHTML = `<h3>Tên</h3>`;
     quantity.innerHTML = `<h3>Số lượng</h3>`;
@@ -22,12 +25,32 @@ function showItem(){
         let j = ingredients[i];
         let totalX = j.total * j.realQuantity / j.quantity;
 
-        index.innerHTML += `<p>${i+1}</p>`
+        edit.innerHTML +=
+        `<p>
+        <div class = "edit${i}">
+            <img class = "remove${i}" src = "images/remove.png">
+            <img class = "pencil${i}" src = "images/pencil.png">
+        </div>
+        </p>
+        `
+        index.innerHTML += `<p class = "id${i}">${i+1}</p>`
         nameX.innerHTML += `<p>${j.name}</p>`
-        quantity.innerHTML += `<p>${j.quantity}</p>`
-        total.innerHTML += `<p>${parseInt(j.total).toLocaleString('en', {useGrouping:true})}đ</p>`
-        realQuantity.innerHTML += `<p>${j.realQuantity}</p>`
 
+        if (j.quantity == ""){
+            quantity.innerHTML += `<p>${j.quantity}</p>`
+        }
+        else{
+            quantity.innerHTML += `<p>${j.quantity} ${j.quantityUnit}</p>`
+        }
+        total.innerHTML += `<p>${parseInt(j.total).toLocaleString('en', {useGrouping:true})}đ</p>`
+
+        if (j.realQuantity == ""){
+            realQuantity.innerHTML += `<p>${j.realQuantity}</p>`
+        }
+        else{
+            realQuantity.innerHTML += `<p>${j.realQuantity} ${j.realQuantityUnit}</p>`
+        }
+        
         if (j.quantity == '' || j.realQuantity == ''){
             realTotal.innerHTML += `<p>${parseInt(j.total).toLocaleString('en', {useGrouping:true})}đ</p>`
             totalCost += parseInt(j.total);
@@ -39,6 +62,9 @@ function showItem(){
     }
 
     document.querySelector(".totalCost").innerHTML = `Total Cost: ${totalCost.toLocaleString('en', {useGrouping:true})}đ`;
+
+    removeItem();
+    editItem()
 }
 
 showItem();
@@ -62,35 +88,115 @@ calculate.addEventListener("click", ()=> {
 
         document.querySelector(".resultQuantity").innerHTML =
         `
-        <h3>Giá tiền: ${totalSell.toFixed(3).toLocaleString('en', {useGrouping:true})}đ </h3>       
+        <h3>Giá tiền: ${totalSell.toLocaleString('en', {useGrouping:true})}đ </h3>       
         `
     }
 })
 
-const addItem = document.querySelector(".addItem button");
+const addItem = document.querySelector(".addButton");
 
 addItem.addEventListener("click", ()=> {
     let nameItem = document.getElementById('nameItem').value;
     let quantityItem = document.getElementById('quantityItem').value;
     let totalItem = document.getElementById('totalItem').value;
     let realQuantityItem = document.getElementById('realQuantityItem').value;
+    let quantityItemUnit = document.getElementById('quantityItemUnit').value;
+    let realQuantityItemUnit = document.getElementById('realQuantityItemUnit').value;
 
-    if ((nameItem != '' && quantityItem == '' && totalItem != '' && realQuantityItem == '') || (nameItem != '' && quantityItem != '' && totalItem != '' && realQuantityItem != '')){
-        ingredients.push(
-        {
-            name: nameItem,
-            quantity: quantityItem,
-            total: totalItem,
-            realQuantity: realQuantityItem
+    if (editing == false){
+        if ((nameItem != '' && quantityItem == '' && totalItem != '' && realQuantityItem == '') || (nameItem != '' && quantityItem != '' && totalItem != '' && realQuantityItem != '')){
+            ingredients.push(
+            {
+                id: ingredients.length,
+                name: nameItem,
+                quantity: quantityItem,
+                quantityUnit: quantityItemUnit,
+                total: totalItem,
+                realQuantity: realQuantityItem,
+                realQuantityUnit: realQuantityItemUnit
+            })
+
+            showItem();
+
+            document.getElementById('nameItem').value = '';
+            document.getElementById('totalItem').value = '';
+            document.getElementById('quantityItem').value = '';
+            document.getElementById('realQuantityItem').value = '';
+        }
+    }
+
+
+})
+
+/*REMOVE ITEM*/
+function removeItem(){
+    for (let i=0; i<ingredients.length; i++){
+        let removeButton = document.querySelector(".remove"+i);
+    
+        removeButton.addEventListener("click", ()=> {
+            ingredients.splice(i, 1);
+            showItem()
         })
+    }
+}
+
+removeItem()
+
+/*EDIT ITEM*/
+const editButton = document.querySelector(".editButton");
+let editIndex = 0;
+
+function editItem(){
+    for (let i=0; i<ingredients.length; i++){
+        let pencilBtn = document.querySelector(".pencil"+i);
+    
+        pencilBtn.addEventListener("click", ()=> {
+            editing = true;
+            editIndex = i;
+            document.getElementById('nameItem').value = ingredients[i].name;
+            addItem.style.display = "none";
+            editButton.style.display = "block";
+
+            for (let j=0; j<ingredients.length; j++){
+                if (i == j){
+                    document.querySelector(".edit" + i).style.background = "green";
+                }
+                else{
+                    document.querySelector(".edit" + j).style.background = "none";
+                }
+            }
+        })
+    }
+}
+       
+editButton.addEventListener("click", ()=> {
+    let nameItem = document.getElementById('nameItem').value;
+    let quantityItem = document.getElementById('quantityItem').value;
+    let totalItem = document.getElementById('totalItem').value;
+    let realQuantityItem = document.getElementById('realQuantityItem').value;
+    let quantityItemUnit = document.getElementById('quantityItemUnit').value;
+    let realQuantityItemUnit = document.getElementById('realQuantityItemUnit').value;
+
+    if (editing == true && (nameItem != '' && quantityItem == '' && totalItem != '' && realQuantityItem == '') || (nameItem != '' && quantityItem != '' && totalItem != '' && realQuantityItem != '')){
+        ingredients[editIndex].name = nameItem;
+        ingredients[editIndex].quantity = quantityItem;
+        ingredients[editIndex].quantityUnit = quantityItemUnit;
+        ingredients[editIndex].total = totalItem;
+        ingredients[editIndex].realQuantity = realQuantityItem;
+        ingredients[editIndex].realQuantityUnit = realQuantityItemUnit;
+
         showItem();
 
         document.getElementById('nameItem').value = '';
         document.getElementById('totalItem').value = '';
         document.getElementById('quantityItem').value = '';
         document.getElementById('realQuantityItem').value = '';
+
+        addItem.style.display = "block";
+        editButton.style.display = "none";
+        editing = false;
     }
-    
-    console.log("ING")
-    console.log(ingredients)
 })
+
+
+
